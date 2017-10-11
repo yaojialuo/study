374075f48b06b95c377176beeff87681dd39e807
yaojialuo <yaojialuo@gmail.com>
2017-10-11 23:16:04 +0800

    debug lstm

--- Chapter08/slice.py
+++ Chapter08/slice.py
@@ -1,4 +1,10 @@
 import tensorflow as tf
+
+sig=tf.Variable([[-0.03335544,  0.09980205,  0.11589614,  0.08302781,  0.26093069]])
+tan=tf.Variable([[ 0.33293939,  0.33492783,  0.73586965,  0.53402126,  0.69271994]])
+sigmoid_i=tf.Variable([[-0.69492048, -0.68598044,  1.02461028,  0.13629574,  0.81286621]])
+mul=sig*tan
+sigmoid=tf.sigmoid(sigmoid_i)
 data = [[[1, 1, 1], [2, 2, 2]],
             [[3, 3, 3], [4, 4, 4]],
             [[5, 5, 5], [6, 6, 6]]]
@@ -8,6 +14,8 @@ clip=tf.clip_by_value(c,6,20)
 with tf.Session() as sess:
     init_op = tf.global_variables_initializer()
     sess.run(init_op)
+    print("sigmoid:",sess.run(sigmoid))
+    print(sess.run(mul))
     print(sess.run(clip))
     print(sess.run(x))
 
--- Chapter08/testLSTM.py
+++ Chapter08/testLSTM.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import reader
 from tensorflow.python import debug as tf_debug
 import numpy as np
+import rnn_cell_impl
 num_hidden=5
 #input=tf.Variable(np.array([1,1,1,1,1]).reshape(5,1))
 input=tf.Variable([[1,1,1,1,1]],dtype=tf.float32)
@@ -12,13 +13,16 @@ with tf.Session() as sess:
 
     #cell = tf.contrib.rnn.LSTMCell(num_hidden,state_is_tuple=False)
     #[[ 0.  0.  0.  0.  0.  0.  0.  0.  0.  0.]]
-    cell = tf.contrib.rnn.BasicLSTMCell(num_hidden, state_is_tuple=True)
+    cell = rnn_cell_impl.BasicLSTMCell(num_hidden, state_is_tuple=True)
 
     #LSTMStateTuple(c=array([[ 0.,  0.,  0.,  0.,  0.]], dtype=float32), h=array([[ 0.,  0.,  0.,  0.,  0.]], dtype=float32))
     initial_state = cell.zero_state(1,input.dtype)
 
     (cell_out, state) = cell(input, initial_state)
-    init_op = tf.initialize_all_variables()
+    init_op = tf.global_variables_initializer()
 
     sess.run(init_op)
+    sess = tf_debug.LocalCLIDebugWrapperSession(sess=sess)
+    writer = tf.summary.FileWriter("E:/Program Files/Anaconda3/envs/tensorflow/Scripts/tensorlog", sess.graph)
+    writer.close()
     print(sess.run([cell_out, state]))
